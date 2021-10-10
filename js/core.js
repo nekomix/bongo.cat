8ad4f18aad276e62174ed28364f42bdae3578706
nekomix <nekomimi.nx@gmail.com>
2021-10-10 18:20:40 +0800

    try

--- js/core.js
+++ js/core.js
@@ -374,3 +374,56 @@ function internationalize() {
 }
 window.addEventListener("languagechange", internationalize);
 document.addEventListener("DOMContentLoaded", internationalize)
+
+$(document).ready(function()  {
+  document.getElementById("playButton").addEventListener("click", function() {
+    document.getElementById("codeModal").style.display = "block";
+  });
+  document.getElementsByClassName("close")[0].addEventListener("click", function() {
+    document.getElementById("codeModal").style.display = "none";
+  });
+  document.getElementById("spaceClear").addEventListener("click", function() {
+    var code = document.getElementById("code").value;
+    var newCode = code.replace(/ /g,'');
+    document.getElementById("code").value = newCode;
+  });
+  document.getElementById("submitButton").addEventListener("click", function() {
+    document.getElementById("codeModal").style.display = "none";
+    var code = document.getElementById("code").value;
+    var timing = parseInt(document.getElementById("timing").value);
+    playCode(code, timing);
+  });
+});
+$(document).ready(function()  {
+  var url_string = window.location.href;
+  var url = new URLSearchParams(url_string);
+  var c = url.get("c");
+  var t = url.get("t");
+  console.log("Code: " + c + " Timing: " + t);
+  if(c != null && t != null) {
+    document.getElementById("playModal").style.display = "block";
+    document.getElementById("playCode").addEventListener("click", function() {
+      document.getElementById("playModal").style.display = "none";
+      playCode(c, t);
+    });
+  }
+});
+function playCode(code, timing) {
+  var length = code.length;
+  var index = 0;
+  var timer = setInterval(function() {
+    index++;
+    var instrument = InstrumentPerKeyEnum[code.charAt(index-1).toUpperCase()];
+    var key = KeyEnum[code.charAt(index-1).toUpperCase()];
+    if(instrument != undefined && key != undefined) {
+      $.play(instrument, key, true);
+      setTimeout(function() {
+        $.play(instrument, key, false);
+      }, timing/2)
+    }
+    if(index >= length) {
+      clearInterval(timer);
+      index = 0;
+    }
+  }, timing );
+}
